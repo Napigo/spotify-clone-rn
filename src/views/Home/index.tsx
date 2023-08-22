b4dd0f9f03a77141bdf3b83b410a226039c9d9a3
Napigo <napigo.hometech@gmail.com>
2023-08-22 11:01:34 +0800

    do some cleaned up at home view

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,10 +1,5 @@
-import React, { useEffect, useState } from "react";
-import {
-  NativeScrollEvent,
-  NativeSyntheticEvent,
-  ScrollView,
-  View,
-} from "react-native";
+import React from "react";
+import { ScrollView, View } from "react-native";
 import { SCREEN_HEIGHT } from "../../theme/constants";
 import { UIText } from "../../components/common/UIText";
 import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
@@ -12,26 +7,12 @@ import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
 export const HomeView: React.FC = () => {
   const height = useBottomTabBarHeight();
 
-  const [isContentUnderBottomBar, setIsContentUnderBottomBar] = useState(false);
-
-  const handleScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
-    const offsetY = event.nativeEvent.contentOffset.y;
-    console.log(offsetY);
-    const isUnderBottomBar = offsetY >= height;
-    setIsContentUnderBottomBar(isUnderBottomBar);
-  };
-
-  useEffect(() => {
-    console.log(isContentUnderBottomBar);
-  }, [isContentUnderBottomBar]);
-
   return (
     <ScrollView
       contentContainerStyle={{
         paddingBottom: height,
       }}
       scrollEventThrottle={30}
-      onScroll={handleScroll}
       style={{ backgroundColor: "D", height: SCREEN_HEIGHT, flex: 1 }}
     >
       <View>
